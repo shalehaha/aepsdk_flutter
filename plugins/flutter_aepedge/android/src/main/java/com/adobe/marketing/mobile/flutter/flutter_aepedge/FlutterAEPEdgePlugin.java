366acb8c93dd5498e1a0b4045021abf1b7577c2f
jgeng <jgeng@adobe.com>
2022-11-15 08:43:05 -0700

    fix compile error for android

--- plugins/flutter_aepedge/android/src/main/java/com/adobe/marketing/mobile/flutter/flutter_aepedge/FlutterAEPEdgePlugin.java
+++ plugins/flutter_aepedge/android/src/main/java/com/adobe/marketing/mobile/flutter/flutter_aepedge/FlutterAEPEdgePlugin.java
@@ -50,7 +50,7 @@ public class FlutterAEPEdgePlugin implements FlutterPlugin, MethodCallHandler {
   }
 
   @Override
-  public void onMethodCall(MethodCall call, Result result) {
+  public void onMethodCall(MethodCall call, final Result result) {
     if ("extensionVersion".equals(call.method)) {
       AndroidUtil.runOnUIThread(new Runnable() {
         @Override
